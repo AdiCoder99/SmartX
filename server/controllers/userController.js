27cb711637e923f07d3e8243f43e66cbf434075c
Aditya Srivastava <aditya4b1@gmail.com>
2026-04-16 00:21:36 +0530

    created contest model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -174,3 +174,4 @@ export const updateUser = async (req, res) => {
 		});
 	}
 };
+
---
+++ server/models/Contest.js
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+// INPUT SCHEMA (for API)
+export const createContestSchema = z.object({
+    title: z.string().min(5),
+    description: z.string().optional(),
+
+    //timing 
+    startTime: z.coerce.date(), // accept ISO string and convert to Date
+    endTime: z.coerce.date(), // accept ISO string and convert to Date
+
+
+    //pricing 
+    entryFee: z.coerce.number().min(0), // accept string and convert to number
+
+    prizePool: z.coerce.number().min(0), // accept string and convert to number
+
+    questions: z.array(
+    z.object({
+        questionId: z.string(), // unique id for each question
+        questionText: z.string().min(5),
+        options: z.array(z.string().min(1)).min(2), // at least 2 options
+        correctOptionIndex: z.number().int().min(0) // index of the correct option
+    })
+    .refine(
+    (q) => q.correctOptionIndex < q.options.length,
+    {
+      message: "correctOptionIndex out of range",
+      path: ["correctOptionIndex"]
+    }
+  )
+    ).min(1),
+
+
+})
+.refine((data) => data.endTime > data.startTime, {
+  message: "endTime must be after startTime",
+  path: ["endTime"]
+}); 
